e78a48b5dcbbd995e72c8e247a9bdfb8fc768bed
Miro Kopecky <kopeckymiroslav@gmail.com>
2016-08-29 21:59:40 +0200

    Robo4j :: JavaOne :: JavaOne4Kids demo update :: license

--- src/main/java/com/javaone/number42/Number42Robot.java
+++ src/main/java/com/javaone/number42/Number42Robot.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright (C) 2016. Miroslav Kopecky
+ * This Number42Robot.java is part of robo4j (http://www.robo4j.io).
+ *
+ *     robo4j-javaone-hand-one is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     robo4j-javaone-hand-one is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with  robo4j (http://www.robo4j.io).  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.javaone.number42;
 
 
@@ -22,7 +40,7 @@ public class Number42Robot {
         robot.setMotor(robotMotor);
         robot.setSensor(robotTouchSensor);
         robot.setRelation(new RobotUnitsRelation(robotTouchSensor,robotMotor));
-        robot.setLCD(new RobotLCD("MAGIC ROBOT"));
+        robot.setLCD(new RobotLCD("JavaOne robot"));
         robot.setPAD(new RobotPAD());
         robot.build();
         if(robot.check()){
